46d45797e6ac5c481cf61b2fef3b6c3134ef9359
Henrique Pinto <126677028+NotPinto15@users.noreply.github.com>
2023-11-24 15:28:23 +0000

    Goal - Defined

--- Assets/MoveToGoalAgent.cs
+++ Assets/MoveToGoalAgent.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
 
 public class MoveToGoalAgent : Agent
 {
+    [SerializeField] private Transform targetTransform;
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(targetTransform.position);
+    }
     public override void OnActionReceived(ActionBuffers actions)
     {
+        Debug.Log(actions.DiscreteActions[0]);
 
     }
-    
+
 }
